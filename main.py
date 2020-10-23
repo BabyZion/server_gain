4c9afe3c9018fd4a52362e25d563c28777baba38
BabyZion <sevcen23@gmail.com>
2020-10-23 21:33:43 +0300

    First commit of a server.

---
+++ main.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python3
+
+import sys
+import socket
+import select
+import parselib
+import time
+import threading
+import binascii
+import libscrc
+from datetime import datetime
+from PyQt5 import QtWidgets, QtCore, Qt
+from window import Ui_MainWindow
+
+
+class Application(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.main_window = Ui_MainWindow()
+        self.main_window.setupUi(self)
+        self.main_window.pushButton.pressed.connect(self.send_gprs_cmd)
+        self.show()
+        self.time_format = '%Y.%m.%d %H:%M:%S.%f'
+        self.server = Server(6969)
+        self.server.received_data.connect(self.parse_data)
+        self.server.new_conn.connect(self.update_imei_box)
+        self.server.start()
+
+    def parse_data(self, data):
+        time_recv = datetime.strftime(datetime.now(), self.time_format)
+        self.main_window.textBrowser.append(f'[{time_recv}] - {data}')
+
+    def update_imei_box(self, imei):
+        self.main_window.comboBox.addItem(imei)
+
+    def send_gprs_cmd(self):
+        cmd = self.main_window.lineEdit.text() + '\r\n'
+        imei = self.main_window.comboBox.currentText()
+        self.server.send_cmd(cmd, imei)
+
+
+class Server(QtCore.QThread):
+
+    received_data = QtCore.pyqtSignal(str)
+    new_conn = QtCore.pyqtSignal(str)
+
+    IMEI_MSG_HEADER = 4
+    TCP_MSG_HEADER = 16
+
+    def __init__(self, port):
+        super().__init__()
+        self.host = '0.0.0.0'
+        self.port = int(port)
+        self.username = "SERVER"
+        self.clients = 0
+        self.clientmap = {}
+        self.conn_threads = []
+        self.time_format = '%Y.%m.%d %H:%M:%S.%f'
+
+    def receive(self, channel, imei=False):
+        full_msg = ''
+        new_msg = True
+        receive = True
+        while receive:
+            msg = channel.recv(64)
+            msg = str(binascii.hexlify(msg))[2:-1]
+            if not msg:
+                return
+            if new_msg:
+                if imei:
+                    #msg = msg[:-1] # Temporary. For debugging. Using netcat.
+                    header_len = self.IMEI_MSG_HEADER
+                    msglen = (int(msg[:header_len], 16) * 2) + 4
+                    new_msg = False
+                else:
+                    header_len = self.TCP_MSG_HEADER
+                    msglen = (int(msg[:header_len], 16) * 2) + 24
+                    new_msg = False
+            full_msg += msg
+            if len(full_msg) >= msglen:
+                receive = False
+                # if not imei:
+                #     #full_msg = full_msg[:-1]
+            # Temporary. For debugging. Using netcat.
+        # print(f"FULL MSG: {full_msg}")
+        return full_msg
+
+    def send(self, channel, msg):
+        if isinstance(msg, str): msg = binascii.unhexlify(msg)
+        channel.send(msg)
+
+    def send_cmd(self, cmd, imei):
+        for soc, conn_imei in self.clientmap.items():
+            if conn_imei == imei:
+                conn = soc
+        cmd = binascii.hexlify(cmd.encode('utf-8')).decode('utf-8')
+        four_zeros = '00000000'
+        codec = '0C'
+        no_of_cmds = '01'
+        type_byte = '05'
+        cmd_len = str(int(len(cmd)/2)).zfill(8)
+        packet = codec + no_of_cmds + type_byte + cmd_len + cmd + no_of_cmds
+        packet_len = hex(int(len(packet) / 2))[2:]
+        packet_len = packet_len.zfill(8)
+        full_packet = four_zeros + packet_len + packet
+        packet_bytes = binascii.unhexlify(packet)
+        crc16 = hex(libscrc.ibm(packet_bytes))[2:].zfill(8)
+        full_packet += crc16
+        full_packet = binascii.unhexlify(full_packet)
+        self.send(conn, full_packet)
+
+    def communicate(self, conn, addr):
+        connected = True
+        imei = None
+        while connected:
+            if not imei:
+                print("Waiting for IMEI...")
+                imei = self.receive(conn, True)
+                imei = binascii.unhexlify(imei[4:]).decode('utf-8')
+                #imei = '000F383634363036303432333339333234'
+                print(imei)
+                if not imei:
+                    connected = False
+                else:
+                    self.send(conn, '01')
+                
+                    self.clientmap[conn] = imei
+                    self.new_conn.emit(imei)
+                    self.received_data.emit(f"Connected from: {addr}. IMEI: {imei}\n")
+            else:
+                data = self.receive(conn)
+                print(data)
+                if data:
+                    packet = (datetime.now(), data)
+                    pinfo = parselib.parse_packet(packet)
+                    rpayload = pinfo['records']
+                    data_no = pinfo['no_of_data_1']
+                    codec = pinfo['codec']
+                    if codec != '0c':
+                        recs = parselib.parse_record_payload(rpayload, data_no, codec)
+                        reply = '0' * (8 - len(data_no)) + data_no
+                        self.received_data.emit(f"{data}")
+                        self.received_data.emit(f"Sending record reply: {reply}")
+                        self.send(conn, reply)
+                    else:
+                        self.received_data.emit(f"{binascii.unhexlify(rpayload[10:]).decode('utf-8')}")
+                else:
+                    connected = False
+
+
+    def run(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((self.host, self.port))
+        self.socket_list = [self.server]
+        self.server.listen(5)
+        running = True
+        while running:
+            conn, addr = self.server.accept()
+            print(f"Connected from {addr}")
+            t = threading.Thread(target=self.communicate, args=[conn, addr])
+            self.conn_threads.append(t)
+            t.start()
+            self.clients += 1
+            
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    aplc = Application()
+    sys.exit(app.exec_())
---
+++ parselib.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+import re
+from datetime import datetime
+
+TCP_PACKET_PATTERN = re.compile(r'Packet len: .*, data: .*\n.*(this is correct single packet|no 0x31 at the end)')
+UDP_PACKET_PATTERN = re.compile(r'Packet len: .*, data: .*\n.*received imei:.*\n.*sending udp')
+PACKET_PATTERN = re.compile(r'((Packet len: .*, data: .*\n.*(this is correct single packet|no 0x31 at the end))|(Packet len: .*, data: .*\n.*received imei:.*\n.*sending udp))')
+DATE_PATTERN = re.compile(r'\d{4}\.\d{2}\.\d{2}\s\d{2}:\d{2}:\d{2}')
+
+def parse_log(log):
+    """
+    Parses supplied string object and finds packets containing record information.
+    Returns the list of said data packets.
+
+        Parameters:
+            log (str): a string representing a log of server_main.
+
+        Returns:
+            packets (list of tuples): returns a list with packets containing the string date of when packet
+            was received and packet data in string format.
+    """
+    packet_matches = PACKET_PATTERN.finditer(log)
+    packets = []
+    for match in packet_matches:
+        match_string = match.group()
+        packet = match.group().split(':')[2].split('\n')[0].strip()
+        time_received = re.search(DATE_PATTERN, match_string).group()
+        time_received = datetime.strptime(time_received, '%Y.%m.%d %H:%M:%S')
+        packets.append((time_received, packet))
+    return packets
+
+def parse_packet(packet):
+    """
+    Goes through supplied string representation of data packet and parses its parts.
+    Considers record as a single part of a packet.
+
+        Parameters:
+            packet (tuple): tuple containing the string date of when packet
+            was received and packet data in string format.
+
+        Returns:
+            packet_info (dict): a dict with various parts of data packet.
+    """
+    time_received, packet = packet
+    if packet[4:8] == 'CAFE':
+        packet_info = __parse_udp_packet(packet)
+    else:
+        packet_info = __parse_tcp_packet(packet)
+    packet_info['time_received'] = time_received
+    return packet_info
+
+def parse_date(log):
+    """
+    Parses supplied string object and finds date strings and returns them.
+
+        Parameters:
+            log (str): a string representing a log of server_main.
+
+        Returns:
+            dates (list): returns a list with string representation of dates found in the log supplied.
+            was received and packet data in string format.
+    """
+    str_dates = DATE_PATTERN.findall(log)
+    dates = [datetime.strptime(d, '%Y.%m.%d %H:%M:%S') for d in str_dates]
+    return dates
+
+def __parse_tcp_packet(packet):
+    """
+    Function used in parse packets function. Specificaly parses TCP packets.
+
+        Parameters:
+            packet (str): a string representation of data packet.
+
+        Returns:
+            packet_info (dict): a dict with various parts of data packet.
+    """
+    rest_of_packet = packet
+    zeros, rest_of_packet = rest_of_packet[:8], rest_of_packet[8:]
+    data_length, rest_of_packet = rest_of_packet[:8], rest_of_packet[8:]
+    codec, rest_of_packet = rest_of_packet[:2], rest_of_packet[2:]
+    no_of_data_1, rest_of_packet = rest_of_packet[:2], rest_of_packet[2:]
+    records, rest_of_packet = rest_of_packet[:-10], rest_of_packet[-10:]
+    no_of_data_2, rest_of_packet = rest_of_packet[:2], rest_of_packet[2:]
+    crc_16 = rest_of_packet
+    packet_info = {'protocol':'TCP', 'zeros':zeros, 'data_length':data_length, 'codec':codec,
+        'no_of_data_1':no_of_data_1, 'records':records, 'no_of_data_2':no_of_data_2, 'crc_16':crc_16}
+    return packet_info
+    
+def __parse_udp_packet(packet):
+    """
+    Function used in parse packets function. Specificaly parses UDP packets.
+
+        Parameters:
+            packet (str): a string representation of data packet.
+
+        Returns:
+            packet_info (dict): a dict with various parts of data packet.
+    """
+    rest_of_packet = packet
+    data_length, rest_of_packet = rest_of_packet[:4], rest_of_packet[4:]
+    identification, rest_of_packet = rest_of_packet[:4], rest_of_packet[4:]
+    not_used, rest_of_packet = rest_of_packet[:2], rest_of_packet[2:]
+    packet_id, rest_of_packet = rest_of_packet[:2], rest_of_packet[2:]
+    imei_len, rest_of_packet = rest_of_packet[:4], rest_of_packet[4:]
+    imei, rest_of_packet = rest_of_packet[:30], rest_of_packet[30:]
+    codec, rest_of_packet = rest_of_packet[:2], rest_of_packet[2:]
+    no_of_data_1, rest_of_packet = rest_of_packet[:2], rest_of_packet[2:]
+    records, rest_of_packet = rest_of_packet[:-2], rest_of_packet[-2:]
+    no_of_data_2 = rest_of_packet
+    packet_info = {'protocol':'UDP', 'data_length':data_length, 'identification':identification, 'not_used':not_used,
+        'packet_id':packet_id, 'imei_len':imei_len, 'imei':imei, 'codec':codec,
+            'no_of_data_1':no_of_data_1, 'records':records, 'no_of_data_2':no_of_data_2}
+    return packet_info
+
+def parse_record_payload(record_payload, no_of_records, codec='08'):
+    """
+    Parses packet payload containing records. Puts every parameter into a dict. Each dict represents a single record.
+    dicts are appended to the list which is than returned.
+
+        Parameters:
+            record_payload (str): a string representation record data.
+            no_of_records (int): a number of records in record_payload.
+            codec (str): type of codec used to encode records. Default '08'.
+
+        Returns:
+            records (list of dicts): a list containing dicts representing records.
+    """
+    records = []
+    rest_of_payload = record_payload
+    mx = 1 if codec == '08' else 2
+    no_of_records = int(no_of_records, 16)
+    recs_parsed = 0
+    while recs_parsed < no_of_records:
+        timestamp, rest_of_payload = rest_of_payload[:16], rest_of_payload[16:]
+        priority, rest_of_payload = rest_of_payload[:2], rest_of_payload[2:]
+        gps_data, rest_of_payload = rest_of_payload[:30], rest_of_payload[30:]
+        event_id, rest_of_payload = rest_of_payload[:2*mx], rest_of_payload[2*mx:]
+        no_of_io, rest_of_payload = rest_of_payload[:2*mx], rest_of_payload[2*mx:]
+        record = {'timestamp':timestamp, 'priority':priority, 'gps_data':gps_data,
+            'event_id':event_id, 'no_of_io':no_of_io}
+        for i in range(4):
+            value_len = (2 ** i) * 2
+            no_of_elements_x_byte, rest_of_payload = rest_of_payload[:2*mx], rest_of_payload[2*mx:]
+            no_of_elements_x_byte = int(no_of_elements_x_byte, 16)
+            for _ in range(no_of_elements_x_byte):
+                avl_id, rest_of_payload = rest_of_payload[:2*mx], rest_of_payload[2*mx:]
+                value, rest_of_payload = rest_of_payload[:value_len], rest_of_payload[value_len:]
+                record[avl_id] = value
+        if codec == '8E':
+            no_of_elements_x_byte, rest_of_payload = rest_of_payload[:2*mx], rest_of_payload[2*mx:]
+            no_of_elements_x_byte = int(no_of_elements_x_byte, 16)
+            for _ in range(no_of_elements_x_byte):
+                avl_id, rest_of_payload = rest_of_payload[:2*mx], rest_of_payload[2*mx:]
+                value_len, rest_of_payload = rest_of_payload[:2*mx], rest_of_payload[2*mx:]
+                value_len = int(value_len, 16) * 2
+                value, rest_of_payload = rest_of_payload[:value_len], rest_of_payload[value_len:]
+                record[avl_id] = value
+        recs_parsed += 1
+        records.append(record)
+    return records
