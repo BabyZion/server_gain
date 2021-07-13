6602d8935adc028a84d34a15f39276ffde44a8af
Dovydas Treinys <treinys.do@ad.teltonika.lt>
2021-07-13 14:51:56 +0300

    Full test statistics calculation implemented.

--- beacon.py
+++ beacon.py
@@ -19,6 +19,7 @@ class Beacon(QtCore.QThread):
         self.check_for_devices = True if not test_devices else False
         self.test_devices = test_devices
         self.check_period = check_period
+        self.full_t_len_results = None
         self.running = False
         self.timer = threading.Timer(self.check_period, self.__query_and_calc)
         self.logger = Logger('Beacon Test')
@@ -42,7 +43,7 @@ class Beacon(QtCore.QThread):
 
     def get_device_data(self):
         checkpoint = datetime.datetime.strftime(self.checkpoint, self.time_format)
-        req = f"SELECT imei, uuid, timestamp, signal_str, beacon_records.id FROM beacon_records FULL OUTER JOIN beacons ON beacon_records.id = beacons.record WHERE timestamp >= '{checkpoint}';"
+        req = f"SELECT imei, uuid, timestamp, signal_str, beacon_records.id FROM beacon_records FULL OUTER JOIN beacons ON beacon_records.id = beacons.record WHERE timestamp >= '{checkpoint}' ORDER BY timestamp;"
         data = self.db.request(req)
         test_data = []
         for datum in data:
@@ -55,6 +56,7 @@ class Beacon(QtCore.QThread):
 
     def calc_stats(self, data):
         results = {}
+        curr_ts = None
         for datum in data:
             if datum[0] in self.test_devices or not datum[0]:
                 if not results.get(datum[0]):
@@ -62,7 +64,9 @@ class Beacon(QtCore.QThread):
                 if not results[datum[0]].get(datum[1]):
                     results[datum[0]][datum[1]] = {'timestamps':[], 'signal_str': []}
                 results[datum[0]][datum[1]]['timestamps'].append(datum[2])
-                results[datum[0]]['all_ts'].append(datum[2])
+                if curr_ts != datum[2] or not curr_ts:
+                    results[datum[0]]['all_ts'].append(datum[2])
+                    curr_ts = datum[2]
                 if datum[3]:
                     results[datum[0]][datum[1]]['signal_str'].append(datum[3])
         for _, res in results.items():
@@ -71,12 +75,62 @@ class Beacon(QtCore.QThread):
 
                 if type(beac) == dict:
                     beac['missing'] = [ts for ts in all_ts if ts not in beac['timestamps']]
+                    del beac['timestamps']
+                    beac['len_missing'] = len(beac['missing'])
                     beac['visible_pct'] = ((len(all_ts) - len(beac['missing'])) / len(all_ts)) * 100
                     if beac['signal_str']:
                         beac['avg_rssi'] = sum([int(x[:-3]) for x in beac['signal_str']]) / len(beac['signal_str'])
+                        del beac['signal_str']
                     else:
                         beac['avg_rssi'] = 0
+            res['len_ts'] = len(res['all_ts'])
+            del res['all_ts']
         return results
+
+    def calc_full_t_len_stats(self, new_data):
+        # new_data = self.__prep_data_for_full_len_calculation(new_data)
+        if not self.full_t_len_results:
+            # Remove 'missing' list
+            for imei, beac_list in new_data.items():
+                for beac, beac_data in beac_list.items():
+                    if type(beac_data) == dict:
+                        del beac_data['missing']
+            self.full_t_len_results = new_data
+        else:
+            for imei, beac_list in new_data.items():
+                # Adds to len_ts count.
+                old_len_ts = self.full_t_len_results[imei]['len_ts']
+                self.full_t_len_results[imei]['len_ts'] += beac_list['len_ts']
+                for beac, beac_data in beac_list.items():
+                    
+                    if type(beac_data) == dict:
+                        # len_missing
+                        old_len_missing = self.full_t_len_results[imei][beac]['len_missing']
+                        self.full_t_len_results[imei][beac]['len_missing'] += beac_data['len_missing']
+                        # visible_pct
+                        missing = self.full_t_len_results[imei][beac]['len_missing']
+                        all_ts = self.full_t_len_results[imei]['len_ts']
+                        self.full_t_len_results[imei][beac]['visible_pct'] = ((all_ts - missing)/all_ts) * 100
+                        # avg_rssi
+                        b = self.full_t_len_results[imei][beac]['avg_rssi']
+                        a = old_len_ts - old_len_missing
+                        a1 = self.full_t_len_results[imei]['len_ts'] - self.full_t_len_results[imei][beac]['len_missing']
+                        b1 = beac_data['avg_rssi'] 
+                        self.full_t_len_results[imei][beac]['avg_rssi'] = (b*a + b1*a1)/(a + a1)
+        # print(self.full_t_len_results)
+        # print('------------------------------------------------------------------')
+        # print(self.__pretty_stats(self.full_t_len_results, 'Full'))
+
+    # def __prep_data_for_full_len_calculation(self, data):
+    #     print(data)
+    #     print()
+    #     for _, res in data.items():
+    #         res['len_all_ts'] = len(res['all_ts'])
+    #         del res['all_ts']
+    #         for _, beac in res.items():
+    #             if type(beac) == dict:
+    #                 beac['len_all_missing'] = len(beac['missing'])
+    #     return data
 
     def __query_and_calc(self):
         if self.check_for_devices:
@@ -87,16 +141,19 @@ class Beacon(QtCore.QThread):
             self.logger.info(self.__pretty_stats(res))
             self.display_info.emit(self.__pretty_stats(res))
             self.checkpoint = datetime.datetime.now()
+            self.calc_full_t_len_stats(res)
             self.logger.info(f"Checkpoint is updated to {datetime.datetime.strftime(self.checkpoint, self.time_format)}")
             self.display_info.emit(f"Checkpoint is updated to {datetime.datetime.strftime(self.checkpoint, self.time_format)}")
+            self.logger.info(self.__pretty_stats(self.full_t_len_results, 'Full'))
+            self.display_info.emit(self.__pretty_stats(self.full_t_len_results, 'Full'))
         else:
             self.logger.warning(f"Couldn't gather statistics. Checkpoint remains at {datetime.datetime.strftime(self.checkpoint, self.time_format)}")
             self.display_info.emit(f"Couldn't gather statistics. Checkpoint remains at {datetime.datetime.strftime(self.checkpoint, self.time_format)}")
         self.timer = threading.Timer(self.check_period, self.__query_and_calc)
         if self.running: self.timer.start()
 
-    def __pretty_stats(self, stats):
-        text = f'Periodic ({self.check_period} s.) Beacon Test results:\n\n'
+    def __pretty_stats(self, stats, res_type='Periodic'):
+        text = f'{res_type} ({self.check_period} s.) Beacon Test results:\n\n'
         # import pdb; pdb.set_trace()
         for imei, beac in stats.items():
             text += f"{imei}:\n"
@@ -105,23 +162,25 @@ class Beacon(QtCore.QThread):
                     if not uuid: uuid = "Empty"
                     text += f"{uuid}:\n"
                     text += f"Avg. Signal Strength: {b['avg_rssi']} dBm\n"
-                    text += f"Visibility: {len(beac['all_ts']) - len(b['missing'])}/{len(beac['all_ts'])}\n"
+                    text += f"Visibility: {beac['len_ts'] - b['len_missing']}/{beac['len_ts']}\n"
                     text += f"Percentage %: {b['visible_pct']}\n"
-                    text += "Missing:\n"
-                    if b['missing']:
-                        try:
-                            for i in range(5):
-                                text += f"{datetime.datetime.strftime(b['missing'][i], self.time_format)}\n"
-                        except IndexError:
-                            pass
-                        text += "...\n"
-                    else:
-                        text += "None\n"
+                    if b.get('missing'):
+                        text += "Missing:\n"
+                        if b['missing']:
+                            try:
+                                for i in range(5):
+                                    text += f"{datetime.datetime.strftime(b['missing'][i], self.time_format)}\n"
+                            except IndexError:
+                                pass
+                            text += "...\n"
+                        else:
+                            text += "None\n"
                 text += '\n'
         return text
 
     def stop(self):
         self.running = False
+        self.full_t_len_results = None
         self.timer.cancel()
 
     def run(self):
