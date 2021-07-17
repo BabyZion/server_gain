cb1366b70e53f6e6bd7c31ef4a68413a1164ec20
Dovydas Treinys <treinys.do@ad.teltonika.lt>
2021-07-17 15:04:38 +0300

    Data visualisation fixes in Beacon module.

--- beacon.py
+++ beacon.py
@@ -46,45 +46,49 @@ class Beacon(QtCore.QThread):
         req = f"SELECT imei, uuid, timestamp, signal_str, beacon_records.id FROM beacon_records FULL OUTER JOIN beacons ON beacon_records.id = beacons.record WHERE timestamp >= '{checkpoint}' ORDER BY timestamp;"
         data = self.db.request(req)
         test_data = []
-        for datum in data:
-            for device in self.test_devices:
-                if device in datum:
-                    test_data.append(datum)
-        data = test_data
-        data = sorted(data, key=lambda x:x[4])
-        return data
+        if data:
+            for datum in data:
+                for device in self.test_devices:
+                    if device in datum:
+                        test_data.append(datum)
+            data = test_data
+            data = sorted(data, key=lambda x:x[4])
+            return data
+        return None
 
     def calc_stats(self, data):
         results = {}
         curr_ts = None
-        for datum in data:
-            if datum[0] in self.test_devices or not datum[0]:
-                if not results.get(datum[0]):
-                    results[datum[0]] = {'all_ts':[]}
-                if not results[datum[0]].get(datum[1]):
-                    results[datum[0]][datum[1]] = {'timestamps':[], 'signal_str': []}
-                results[datum[0]][datum[1]]['timestamps'].append(datum[2])
-                if curr_ts != datum[2] or not curr_ts:
-                    results[datum[0]]['all_ts'].append(datum[2])
-                    curr_ts = datum[2]
-                if datum[3]:
-                    results[datum[0]][datum[1]]['signal_str'].append(datum[3])
-        for _, res in results.items():
-            all_ts = res['all_ts']
-            for _, beac in res.items():
+        if data:
+            for datum in data:
+                if datum[0] in self.test_devices or not datum[0]:
+                    if not results.get(datum[0]):
+                        results[datum[0]] = {'all_ts':[]}
+                    if not results[datum[0]].get(datum[1]):
+                        results[datum[0]][datum[1]] = {'timestamps':[], 'signal_str': []}
+                    results[datum[0]][datum[1]]['timestamps'].append(datum[2])
+                    if curr_ts != datum[2] or not curr_ts:
+                        results[datum[0]]['all_ts'].append(datum[2])
+                        curr_ts = datum[2]
+                    if datum[3]:
+                        results[datum[0]][datum[1]]['signal_str'].append(datum[3])
+            for _, res in results.items():
+                all_ts = res['all_ts']
+                for _, beac in res.items():
 
-                if type(beac) == dict:
-                    beac['missing'] = [ts for ts in all_ts if ts not in beac['timestamps']]
-                    del beac['timestamps']
-                    beac['len_missing'] = len(beac['missing'])
-                    beac['visible_pct'] = ((len(all_ts) - len(beac['missing'])) / len(all_ts)) * 100
-                    if beac['signal_str']:
-                        beac['avg_rssi'] = sum([int(x[:-3]) for x in beac['signal_str']]) / len(beac['signal_str'])
-                        del beac['signal_str']
-                    else:
-                        beac['avg_rssi'] = 0
-            res['len_ts'] = len(res['all_ts'])
-            del res['all_ts']
+                    if type(beac) == dict:
+                        beac['missing'] = [ts for ts in all_ts if ts not in beac['timestamps']]
+                        del beac['timestamps']
+                        beac['len_missing'] = len(beac['missing'])
+                        beac['all_ts'] = len(all_ts)
+                        beac['visible_pct'] = ((beac['all_ts'] - len(beac['missing'])) / beac['all_ts']) * 100
+                        if beac['signal_str']:
+                            beac['avg_rssi'] = sum([int(x[:-3]) for x in beac['signal_str']]) / len(beac['signal_str'])
+                            del beac['signal_str']
+                        else:
+                            beac['avg_rssi'] = 0
+                res['len_ts'] = len(res['all_ts'])
+                del res['all_ts']
         return results
 
     def calc_full_t_len_stats(self, new_d):
@@ -99,70 +103,77 @@ class Beacon(QtCore.QThread):
         else:
             for imei, beac_list in new_data.items():
                 # Adds to len_ts count.
-                old_len_ts = self.full_t_len_results[imei]['len_ts']
                 self.full_t_len_results[imei]['len_ts'] += beac_list['len_ts']
                 for beac, beac_data in beac_list.items():
                     
                     if type(beac_data) == dict:
                         if self.full_t_len_results[imei].get(beac):
+                            # len_ts
+                            old_len_ts = self.full_t_len_results[imei][beac]['all_ts']
+                            self.full_t_len_results[imei][beac]['all_ts'] += beac_data['all_ts']
                             # len_missing
                             old_len_missing = self.full_t_len_results[imei][beac]['len_missing']
                             self.full_t_len_results[imei][beac]['len_missing'] += beac_data['len_missing']
                             # visible_pct
                             missing = self.full_t_len_results[imei][beac]['len_missing']
-                            all_ts = self.full_t_len_results[imei]['len_ts']
+                            all_ts = self.full_t_len_results[imei][beac]['all_ts']
                             self.full_t_len_results[imei][beac]['visible_pct'] = ((all_ts - missing)/all_ts) * 100
                             # avg_rssi
                             b = self.full_t_len_results[imei][beac]['avg_rssi']
                             a = old_len_ts - old_len_missing
-                            a1 = self.full_t_len_results[imei]['len_ts'] - self.full_t_len_results[imei][beac]['len_missing']
+                            a1 = self.full_t_len_results[imei][beac]['all_ts'] - self.full_t_len_results[imei][beac]['len_missing']
                             b1 = beac_data['avg_rssi'] 
                             self.full_t_len_results[imei][beac]['avg_rssi'] = (b*a + b1*a1)/(a + a1)
                         else:
                             self.full_t_len_results[imei][beac] = {}
+                            self.full_t_len_results[imei][beac]['all_ts'] = beac_data['all_ts']
                             self.full_t_len_results[imei][beac]['len_missing'] = beac_data['len_missing']
                             self.full_t_len_results[imei][beac]['visible_pct'] = beac_data['visible_pct']
                             self.full_t_len_results[imei][beac]['avg_rssi'] = beac_data['avg_rssi']
-
-    
+                # Increase 'all_ts' of remaining beacons that have already been visible but not during this period
+                for beacon in self.full_t_len_results[imei]:
+                    if beacon not in beac_list:
+                       self.full_t_len_results[imei][beacon]['all_ts'] += beac_list['len_ts']
+                       self.full_t_len_results[imei][beacon]['len_missing'] += beac_list['len_ts']
+                    
     def write_results_to_files(self, periodic_stats, full_stats):
-        for imei, beac_list in periodic_stats.items():
-            len_ts = beac_list['len_ts']
-            for beac, data in beac_list.items():
-                try:
-                    for m in data['missing']:
-                        mis = {'imei':imei, 'beacon':beac, 'date':datetime.datetime.strftime(m, self.time_format)}
-                        with open(self.test_missing_file, 'a') as mf:
-                            writer = csv.DictWriter(mf, fieldnames=mis.keys())
-                            if os.stat(self.test_missing_file).st_size == 0:
+        try:
+            for imei, beac_list in periodic_stats.items():
+                for beac, data in beac_list.items():
+                    try:
+                        for m in data['missing']:
+                            mis = {'imei':imei, 'beacon':beac, 'date':datetime.datetime.strftime(m, self.time_format)}
+                            with open(self.test_missing_file, 'a') as mf:
+                                writer = csv.DictWriter(mf, fieldnames=mis.keys())
+                                if os.stat(self.test_missing_file).st_size == 0:
+                                    writer.writeheader()
+                                writer.writerow(mis)
+                    except TypeError:
+                        pass
+                    if type(data) == dict:
+                        del data['missing']
+                        data['imei'] = imei
+                        data['beacon'] = beac
+                        data['stats'] = 'periodic'
+                        with open(self.test_res_file, 'a') as f:
+                            writer = csv.DictWriter(f, fieldnames=data.keys())
+                            if os.stat(self.test_res_file).st_size == 0:
                                 writer.writeheader()
-                            writer.writerow(mis)
-                except TypeError:
-                    pass
-                if type(data) == dict:
-                    del data['missing']
-                    data['imei'] = imei
-                    data['beacon'] = beac
-                    data['len_ts'] = len_ts
-                    data['stats'] = 'periodic'
-                    with open(self.test_res_file, 'a') as f:
-                        writer = csv.DictWriter(f, fieldnames=data.keys())
-                        if os.stat(self.test_res_file).st_size == 0:
-                            writer.writeheader()
-                        writer.writerow(data)
-        for imei, beac_list in full_stats.items():        
-            len_ts = beac_list['len_ts']
-            for beac, data in beac_list.items():
-                if type(data) == dict:
-                    data['imei'] = imei
-                    data['beacon'] = beac
-                    data['len_ts'] = len_ts
-                    data['stats'] = 'full'
-                    with open(self.test_res_file, 'a') as f:
-                        writer = csv.DictWriter(f, fieldnames=data.keys())
-                        writer.writerow(data)
-        self.logger.info("Results are written to csv file.")
-        self.display_info.emit("Results are written to csv file.")
+                            writer.writerow(data)
+            for imei, beac_list in full_stats.items():        
+                for beac, data in beac_list.items():
+                    if type(data) == dict:
+                        data['imei'] = imei
+                        data['beacon'] = beac
+                        data['stats'] = 'full'
+                        with open(self.test_res_file, 'a') as f:
+                            writer = csv.DictWriter(f, fieldnames=data.keys())
+                            writer.writerow(data)
+            self.logger.info("Results are written to csv file.")
+            self.display_info.emit("Results are written to csv file.")
+        except PermissionError as e:
+            self.logger.info("Results of this period couldn't be written to file - {e}")
+            self.display_info.emit("Results of this period couldn't be written to file - {e}")
 
     def __query_and_calc(self):
         try:
@@ -189,30 +200,35 @@ class Beacon(QtCore.QThread):
             self.logger.error(traceback.format_exc())
 
     def __pretty_stats(self, stats, res_type='Periodic'):
-        text = f'{res_type} ({self.check_period} s.) Beacon Test results:\n\n'
-        # import pdb; pdb.set_trace()
-        for imei, beac in stats.items():
-            text += f"{imei}:\n"
-            for uuid, b in beac.items():
-                if type(b) == dict:
-                    if not uuid: uuid = "Empty"
-                    text += f"{uuid}:\n"
-                    text += f"Avg. Signal Strength: {b['avg_rssi']} dBm\n"
-                    text += f"Visibility: {beac['len_ts'] - b['len_missing']}/{beac['len_ts']}\n"
-                    text += f"Percentage %: {b['visible_pct']}\n"
-                    if b.get('missing'):
-                        text += "Missing:\n"
-                        if b['missing']:
-                            try:
-                                for i in range(5):
-                                    text += f"{datetime.datetime.strftime(b['missing'][i], self.time_format)}\n"
-                            except IndexError:
-                                pass
-                            text += "...\n"
-                        else:
-                            text += "None\n"
-                text += '\n'
-        return text
+        try:
+            text = f'{res_type} ({self.check_period} s.) Beacon Test results:\n\n'
+            # import pdb; pdb.set_trace()
+            for imei, beac in stats.items():
+                text += f"{imei}:\n"
+                for uuid, b in beac.items():
+                    if type(b) == dict:
+                        if not uuid: uuid = "Empty"
+                        text += f"{uuid}:\n"
+                        text += f"Avg. Signal Strength: {b['avg_rssi']} dBm\n"
+                        text += f"Visibility: {b['all_ts'] - b['len_missing']}/{b['all_ts']}\n"
+                        text += f"Percentage %: {b['visible_pct']}\n"
+                        if b.get('missing'):
+                            text += "Missing:\n"
+                            if b['missing']:
+                                try:
+                                    for i in range(5):
+                                        text += f"{datetime.datetime.strftime(b['missing'][i], self.time_format)}\n"
+                                except IndexError:
+                                    pass
+                                text += "...\n"
+                            else:
+                                text += "None\n"
+                    text += '\n'
+            return text
+        except KeyError:
+            print(traceback.format_exc())
+            print(stats)
+            os.exit(1)
 
     def stop(self):
         self.running = False
