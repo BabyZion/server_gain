7a6112ebef034dda811aaa800d40cfa88d9b9eb0
BabyZion <sevcen23@gmail.com>
2021-07-19 21:01:29 +0300

    Database initialization script added.

---
+++ init_db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE beacon_records (
+id SERIAL NOT NULL PRIMARY KEY,
+data text NOT NULL,
+ts timestamp NOT NULL,
+imei VARCHAR(15) NOT NULL);
+
+CREATE TABLE beacons (
+id SERIAL NOT NULL PRIMARY KEY,
+uuid VARCHAR(40) NOT NULL,
+record INT NOT NULL REFERENCES beacon_records(id),
+beacon_flag VARCHAR(8),
+signal_str VARCHAR(2),
+batt_v VARCHAR(4),
+temp VARCHAR(4),
+param_01 VARCHAR(31),
+param_02 VARCHAR(31),
+param_03 VARCHAR(31),
+add_data VARCHAR(31));
