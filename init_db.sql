23c19408660c6c0c5cc48170e4a272730d37fc28
Dovydas Treinys <treinys.do@ad.teltonika.lt>
2021-07-21 21:20:41 +0300

    Fixed errors in init_db.sql file.

--- init_db.sql
+++ init_db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE beacon_records (
 id SERIAL NOT NULL PRIMARY KEY,
 data text NOT NULL,
-ts timestamp NOT NULL,
+timestamp timestamp NOT NULL,
 imei VARCHAR(15) NOT NULL);
 
 CREATE TABLE beacons (
@@ -9,7 +9,7 @@ id SERIAL NOT NULL PRIMARY KEY,
 uuid VARCHAR(40) NOT NULL,
 record INT NOT NULL REFERENCES beacon_records(id),
 beacon_flag VARCHAR(8),
-signal_str VARCHAR(2),
+signal_str VARCHAR(8),
 batt_v VARCHAR(4),
 temp VARCHAR(4),
 param_01 VARCHAR(31),
